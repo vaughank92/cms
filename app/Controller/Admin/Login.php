2ead306382c51743ca94a1a68e8beb6efc038141
Thomas Slade <thomas@blueacorn.com>
2014-06-11 15:52:00 -0400

    Started building out admin functionality

--- app/Controller/Admin/Login.php
+++ app/Controller/Admin/Login.php
@@ -6,13 +6,19 @@
  * Time: 11:26 AM
  */
 
-class Controller_Admin_Login {
+class Controller_Admin_Login extends Controller_Admin_Abstract {
 
     public $loggedIn;
 
     function __construct()
     {
 
+    }
+
+    public function postAction(){
+        //Check username and password
+        //Set sessions vars to persist
+        //Set cookie
     }
 
 
